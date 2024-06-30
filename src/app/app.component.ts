2bbfdd9fb16bf4f1da1cd2fa29aade5ba776fd01
Your Name <safynazabdelraheem@gmail.com>
2024-06-30 18:56:45 +0300

    signup,reset password,login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { RegisterTwoComponent } from './components/register-two/register-two.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PasswordComponent } from './components/password/password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RegisterComponent,RegisterTwoComponent,RouterOutlet, ReactiveFormsModule,CommonModule,SignInComponent, HttpClientModule,PasswordComponent,ResetPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { RegisterTwoComponent } from './components/register-two/register-two.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+import { PasswordComponent } from './components/password/password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  // { path: '', redirectTo: '/register', pathMatch: 'full' } , 
+  { path: 'two', component: RegisterTwoComponent },
+  { path: 'sign', component: SignInComponent },
+  { path: 'pass', component: PasswordComponent },
+  { path: 'rest', component: ResetPasswordComponent }
+
+];
