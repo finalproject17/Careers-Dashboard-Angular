1cf1568e934e0609622fe7a3ea46eeba6a1d263c
salmahammad436 <sayahammad@gmail.com>
2024-07-01 23:02:13 +0300

    01.07.2024

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,6 +4,6 @@ export const environment = {
        githubClientId: '57f2b47117dcdeddcd33',  
    githubRedirectUri: 'http://localhost:4200/register',
 
-    //  baseUrl : "https://deploying-backend-taupe.vercel.app"
-    baseUrl:'http://localhost:3001', 
+     baseUrl : "https://deploying-backend-taupe.vercel.app"
+   // baseUrl:'http://localhost:3001', 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
       production: false,
       githubClientId: '57f2b47117dcdeddcd33',  
   githubRedirectUri: 'http://localhost:4200/register',
-    // baseUrl : "https://deploying-backend-taupe.vercel.app"
-    baseUrl:'http://localhost:3001', 
+     baseUrl : "https://deploying-backend-taupe.vercel.app"
+    //baseUrl:'http://localhost:3001', 
 };
